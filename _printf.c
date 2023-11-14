745c22f91ebcfe1d489ddd4e37287fc6adb5c14d
6lackAli3n <abubakarabdullahayo26@gmail.com>
2023-11-14 22:28:17 +0100

    printf

--- _printf.c
+++ _printf.c
@@ -1,60 +1,10 @@
 #include "main.h"
-#include <stdarg.h>
 #include <unistd.h>
-/**
- * Count - the number of digits in an integer.
- *
- * @param n: The integer for which to count digits.
- * @return: The number of digits in the integer.
- */
-int num_digits(int n)
-{
-	int count = 0;
-
-	while (n != 0)
-	{
-		n /= 10;
-		count++;
-	}
-	return (count);
-}
-
-/**
- * Print - an integer to the standard output.
- *
- * @param n: The integer to be printed.
- * @return: The number of digits in the printed integer.
- */
-int print_integer(int n)
-{
-	int digits = num_digits(n);
-	int divisor, digit;
-
-	if (n < 0)
-	{
-		_putchar('-');
-		n = -n;
-	}
-
-	while (digits > 0)
-	{
-		divisor = 1;
-		for (int i = 1; i < digits; i++)
-		{
-			divisor *= 10;
-		}
-		digit = n / divisor;
-		_putchar(digit + '0');
-		n %= divisor;
-		digits--;
-	}
-	return (num_digits(n));
-}
+#include <stdarg.h>
 
 /**
  * _printf - Custom printf function
- * @format: Format string
- * @...: Variable number of arguments
+ * @format: Format string containing conversion specifiers
  *
  * Return: Number of characters printed (excluding null byte)
  */
@@ -73,57 +23,69 @@ int _printf(const char *format, ...)
 			switch (*format)
 			{
 				case 'c':
-					count += _putchar(va_arg(args, int));
+					{
+						char c = va_arg(args, int);
+						write(1, &c, 1);
+						count++;
+					}
 					break;
 				case 's':
-					count += _puts(va_arg(args, char *));
+					{
+						char *str = va_arg(args, char *);
+						while (*str)
+						{
+							write(1, str, 1);
+							str++;
+							count++;
+						}
+					}
+					break;
+				case '%':
+					write(1, "%", 1);
+					count++;
 					break;
 				case 'd':
 				case 'i':
-					count += print_integer(va_arg(args, int));
-					break;
-				case '%':
-					count += _putchar('%');
+					{
+						int num = va_arg(args, int);
+						int digits = 0;
+						char buffer[20];
+						
+						if (num < 0)
+						{
+							write(1, "-", 1);
+							count++;
+							num = -num;
+						}
+						do
+						{
+							buffer[digits++] = num % 10 + '0';
+							num /= 10;
+						} while (num != 0);
+
+						while (digits > 0)
+						{
+							write(1, &buffer[--digits], 1);
+							count++;
+						}
+					}
 					break;
 				default:
-					break;
+					write(1, "%", 1);
+					write(1, format, 1);
+					count += 2;
 			}
 		}
 		else
 		{
-			count += _putchar(*format);
+			write(1, format, 1);
+			count++;
 		}
+
 		format++;
 	}
+
 	va_end(args);
-	return (count);
-}
 
-/**
- * _putchar - Write a character to stdout
- * @c: Character to write
- *
- * Return: 1 on success, -1 on failure
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
-/**
- * _puts - Write a string to stdout
- * @str: String to write
- *
- * Return: Number of characters written
- */
-int _puts(char *str)
-{
-	int count = 0;
-
-	while (*str)
-	{
-		count += _putchar(*str);
-		str++;
-	}
-	return (count);
+	return count;
 }
