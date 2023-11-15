f2e2303f90fb8345cb076a4cc34c5b41a3bc3d73
6lackAli3n <abubakarabdullahayo26@gmail.com>
2023-11-15 16:27:52 +0100

    printf

--- _printf.c
+++ _printf.c
@@ -1,185 +1,74 @@
 #include "main.h"
-#include <unistd.h>
 #include <stdarg.h>
+#include <unistd.h>
 
 /**
  * _printf - Custom printf function
- * @format: Format string containing conversion specifiers
+ * @format: Format string
  *
  * Return: Number of characters printed (excluding null byte)
  */
 int _printf(const char *format, ...)
 {
-	int count = 0;
 	va_list args;
+	int count = 0;
 
 	va_start(args, format);
 
 	while (*format)
 	{
-		if (*format == '%')
+		if (*format == '%' && (*(format + 1) == 'c' || *(format + 1) == 's' || *(format + 1) == '%'))
 		{
-			format++;
-			switch (*format)
-			{
-				case 'c':
-					{
-						char c = va_arg(args, int);
-
-						write(1, &c, 1);
-						count++;
-					}
-					break;
-				case 's':
-					{
-						char *str = va_arg(args, char *);
-						while (*str)
-						{
-							write(1, str, 1);
-							str++;
-							count++;
-						}
-					}
-					break;
-				case '%':
-					write(1, "%", 1);
-					count++;
-					break;
-				case 'd':
-				case 'i':
-					{
-						int num = va_arg(args, int);
-						int digits = 0;
-						char buffer[20];
-
-						if (num < 0)
-						{
-							write(1, "-", 1);
-							count++;
-							num = -num;
-						}
-						do {
-							buffer[digits++] = num % 10 + '0';
-							num /= 10;
-						} while (num != 0);
-
-						while (digits > 0)
-						{
-							write(1, &buffer[--digits], 1);
-							count++;
-						}
-					}
-					break;
-				case 'u':
-					{
-						unsigned int num = va_arg(args, unsigned int);
-						int digits = 0;
-						char buffer[20];
-
-						do {
-							buffer[digits++] = num % 10 + '0';
-							num /= 10;
-						} while (num != 0);
-
-						while (digits > 0)
-						{
-							write(1, &buffer[--digits], 1);
-							count++;
-						}
-					}
-					break;
-				case 'o':
-					{
-						unsigned int num = va_arg(args, unsigned int);
-						int digits = 0;
-						char buffer[20];
-
-						do {
-							buffer[digits++] = num % 8 + '0';
-							num /= 8;
-						} while (num != 0);
-
-						while (digits > 0)
-						{
-							write(1, &buffer[--digits], 1);
-							count++;
-						}
-					}
-					break;
-				case 'x':
-					{
-						unsigned int num = va_arg(args, unsigned int);
-						int digits = 0;
-						char buffer[20];
-
-						do {
-							int remainder = num % 16;
-							buffer[digits++] = (remainder < 10) ? remainder + '0' : remainder - 10 + 'a';
-							num /= 16;
-						} while (num != 0);
-
-						while (digits > 0)
-						{
-							write(1, &buffer[--digits], 1);
-							count++;
-						}
-					}
-					break;
-				case 'X':
-					{
-						unsigned int num = va_arg(args, unsigned int);
-						int digits = 0;
-						char buffer[20];
-
-						do {
-							int remainder = num % 16;
-
-							buffer[digits++] = (remainder < 10) ? remainder + '0' : remainder - 10 + 'A';
-							num /= 16;
-						} while (num != 0);
-
-						while (digits > 0)
-						{
-							write(1, &buffer[--digits], 1);
-							count++;
-						}
-					}
-					break;
-				case 'b':
-					{
-						unsigned int num = va_arg(args, unsigned int);
-						int bits[32];
-						int index = 0;
-
-						while (num > 0)
-						{
-							bits[index++] = num % 2;
-							num /= 2;
-						}
-						while (--index >= 0)
-						{
-							char bit = bits[index] + '0';
-							write(1, &bit, 1);
-							count++;
-						}
-					}
-					break;
-				default:
-					write(1, "%", 1);
-					write(1, format, 1);
-					count += 2;
-			}
+			count += handle_format(args, *(format + 1));
+			format += 2;
 		}
 		else
 		{
-			write(1, format, 1);
+			write(1, format++, 1);
 			count++;
 		}
-
-		format++;
 	}
 
 	va_end(args);
 
 	return (count);
+}
+
+/**
+ * handle_format - Handle conversion specifiers
+ * @args: Variable arguments list
+ * @specifier: Conversion specifier
+ *
+ * Return: Number of characters printed for the specifier
+ */
+int handle_format(va_list args, char specifier)
+{
+	int len = 0;
+
+	if (specifier == 'c')
+	{
+		char c = va_arg(args, int);
+
+		write(1, &c, 1);
+
+		return (1);
+	}
+	else if (specifier == 's')
+	{
+		char *str = va_arg(args, char *);
+
+		if (str == NULL)
+			str = "(null)";
+
+		while (str[len])
+			len++;
+		write(1, str, len);
+		return (len);
+	}
+	else if (specifier == '%')
+	{
+		write(1, "%", 1);
+		return (1);
+	}
+	return (0);
 }
--- main.h
+++ main.h
@@ -7,5 +7,6 @@ int _printf(const char *format, ...);
 int _putchar(char c);
 int _puts(char *str);
 int my_print_number(int n);
+int handle_format(va_list args, char specifier);
 
 #endif /* MAIN_H */
