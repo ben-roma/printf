60718cb6e060a4c6ef5f7bec3983f732ce274329
6lackAli3n <abubakarabdullahayo26@gmail.com>
2023-11-15 18:40:58 +0100

    printf

--- _printf.c
+++ _printf.c
@@ -99,23 +99,25 @@ int handle_format(va_list args, char specifier)
 	if (specifier == 'c')
 	{
 		char c = va_arg(args, int);
+
 		write(1, &c, 1);
-		return 1;
+		return (1);
 	}
 	else if (specifier == 's')
 	{
 		char *str = va_arg(args, char *);
+
 		if (str == NULL)
 			str = "(null)";
 		while (str[len])
 			len++;
 		write(1, str, len);
-		return len;
+		return (len);
 	}
 	else if (specifier == '%')
 	{
 		write(1, "%", 1);
-		return 1;
+		return (1);
 	}
-	return 0;
+	return (0);
 }
