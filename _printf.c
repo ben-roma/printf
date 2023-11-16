3151eefef56854a2433ede2a536b09db22a4bd0f
6lackAli3n <abubakarabdullahayo26@gmail.com>
2023-11-16 16:28:24 +0100

    printf

--- _printf.c
+++ _printf.c
@@ -48,6 +48,36 @@ int print_number(int n)
 	count = print_str(buffer);
 	return (count);
 }
+/**
+ * print_binary - Print a number in binary
+ * @n: Number to print
+ *
+ * Return: Number of characters printed
+ */
+int print_binary(unsigned int n)
+{
+	int count = 0;
+	char binary[32];
+	int i;
+
+	if (n == 0)
+	{
+		count += print_char('0');
+	}
+	else
+	{
+		for (i = 0; n > 0; i++)
+		{
+			binary[i] = (n % 2) + '0';
+			n /= 2;
+		}
+		for (i = i - 1; i >= 0; i--)
+		{
+			count += write(1, &binary[i], 1);
+		}
+	}
+	return (count);
+}
 int _printf(const char *format, ...)
 {
 	va_list args;
@@ -56,6 +86,7 @@ int _printf(const char *format, ...)
 	char c;
 	char *str;
 	int num;
+	unsigned int ub;
 
 	va_start(args, format);
 
@@ -80,6 +111,10 @@ int _printf(const char *format, ...)
 				case 'i':
 					num = va_arg(args, int);
 					count += print_number(num);
+					break;
+				case 'b':
+					ub = va_arg(args, unsigned int);
+					count += print_binary(ub);
 					break;
 				case '%':
 					count += write(1, "%", 1);
